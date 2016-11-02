c07de21f3f415bc1964c7c84390a4753c4389f37
Vitor Emanuel <sktvitex@gmail.com>
2016-11-02 21:02:15 -0200

    capitulo04-aula01 [v2]

---
+++ app/pages/generated-test/generated-test.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  templateUrl: 'build/pages/generated-test/generated-test.html',
+})
+export class GeneratedTestPage {
+  constructor(private nav: NavController) {}
+}
